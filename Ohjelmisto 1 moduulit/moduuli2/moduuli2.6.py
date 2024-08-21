956fc0c32c082a563286413bdc00d7ee039a8578
Teppo Toropainen <toropainenteppo@gmail.com>
2024-08-21 22:55:20 +0300

    moduulin ratkaisu

--- Ohjelmisto 1 moduulit/moduuli2/moduuli2.6.py
+++ Ohjelmisto 1 moduulit/moduuli2/moduuli2.6.py
@@ -7,9 +7,10 @@ r3 = random.randint(0,9)
 r4 = random.randint(1,6)
 r5 = random.randint(1,6)
 r6 = random.randint(1,6)
+r7 = random.randint(1,6)
 
 koodi3numeroa = r1, r2, r3
-koodi4numeroa = r4, r5, r6
+koodi4numeroa = r4, r5, r6, r7
 
 print(f"3 numeroinen koodi on: {koodi3numeroa}")
 print(f"4 numeroinen koodi on: {koodi4numeroa}")
