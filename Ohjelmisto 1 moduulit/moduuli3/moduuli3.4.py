2074b858a1b00c17c3c31aec394a42fba385597a
Teppo Toropainen <toropainenteppo@gmail.com>
2024-08-23 10:24:07 -0700

    moduulin ratkaisu

--- Ohjelmisto 1 moduulit/moduuli3/moduuli3.4.py
+++ Ohjelmisto 1 moduulit/moduuli3/moduuli3.4.py
@@ -1,10 +1,12 @@
 vuosiluku = float(input("Vuosiluku: "))
 
 if vuosiluku %4 == 0:
-    print(f"{vuosiluku:} on karkausvuosi")
-elif vuosiluku %100 == 0:
-    print(f"{vuosiluku:} ei ole karkausvuosi")
-elif vuosiluku %400 == 0:
-    print(f"{vuosiluku:} on karkausvuosi")
+    if vuosiluku % 400 == 0:
+        print("on karkausvuosi")
+    elif vuosiluku % 100 == 0:
+        print("ei ole karkausvuosi")
+    else:
+        print("on karkausvuosi")
 else:
-    print("Vuosi ei ole karkausvuosi")
+    print("ei ole karkausvuosi")
+
