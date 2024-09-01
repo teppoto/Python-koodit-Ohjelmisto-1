6d72a7519196c43063d28a72a814e02bf6a599b9
Teppo Toropainen <toropainenteppo@gmail.com>
2024-09-01 20:15:03 +0300

    moduulin ratkaisu

---
+++ Ohjelmisto 1 moduulit/moduuli5/moduuli5.1.py
@@ -0,0 +1,12 @@
+""" Kirjoita ohjelma, joka kysyy käyttäjältä arpakuutioiden lukumäärän.
+    Ohjelma heittää kerran kaikkia arpakuutioita ja tulostaa silmälukujen summan.
+    Käytä for-toistorakennetta."""
+
+arpakuutiot = print(input("Arpakuutioiden lukumaara: "))
+summa = int(arpakuutiot)
+
+while arpakuutiot
+
+
+for
+    print(summa)
