ab5b8284e8d7fc0435dcfb384de9d2d2be683c8b
Teppo Toropainen <toropainenteppo@gmail.com>
2024-09-03 11:36:26 -0700

    moduulin ratkaisu

---
+++ Ohjelmisto 1 moduulit/moduuli5/moduuli5.3.py
@@ -0,0 +1,8 @@
+
+
+kokonaisluku = int(input("Anna kokonaisluku: "))
+
+if kokonaisluku == kokonaisluku / 1 and kokonaisluku / kokonaisluku:
+    print(f"{kokonaisluku} on kokonaisluku")
+elif kokonaisluku != kokonaisluku / 1 or kokonaisluku / kokonaisluku:
+    print(f"{kokonaisluku} ei ole kokonaisluku")
