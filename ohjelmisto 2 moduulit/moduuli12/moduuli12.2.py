807351311d422ff3da385b286d07c077a8aaab4d
Teppo Toropainen <toropainenteppo@gmail.com>
2024-10-29 13:24:32 -0700

    moduulin ratkaisu

--- ohjelmisto 2 moduulit/moduuli12/moduuli12.2.py
+++ ohjelmisto 2 moduulit/moduuli12/moduuli12.2.py
@@ -3,12 +3,12 @@ import requests
 
 #paikkakunta = input("Municipality name: ")
 
-API_KEY = "d67faaf3b5b316fa5f56784009ac06ef"
+API_KEY =
 
-#geo = "http://api.openweathermap.org/geo/1.0/direct?q=paikkakunta,{state code},{country code}&limit={limit}&appid={d10b1e5c8f49ac3289bb0c1e29bbb096}"
-#saa = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={d10b1e5c8f49ac3289bb0c1e29bbb096}"
+#geo = "http://api.openweathermap.org/geo/1.0/direct?q=paikkakunta,{state code},{country code}&limit={limit}&appid={}"
+#saa = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={}"
 
-joku = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={d67faaf3b5b316fa5f56784009ac06ef}"
+joku = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={}"
 
 vastaus = requests.get(joku).json()
 print(vastaus)
