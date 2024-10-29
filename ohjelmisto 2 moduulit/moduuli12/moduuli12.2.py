205685df28069c9a7a163dc64d1bcebbe7a98083
Teppo Toropainen <toropainenteppo@gmail.com>
2024-10-29 17:10:13 +0200

    moduulin ratkaisu

--- ohjelmisto 2 moduulit/moduuli12/moduuli12.2.py
+++ ohjelmisto 2 moduulit/moduuli12/moduuli12.2.py
@@ -1,14 +1,30 @@
 """Kirjoita ohjelma, joka kysyy käyttäjältä paikkakunnan nimen ja tulostaa sitä vastaavan säätilan tekstin sekä lämpötilan Celsius-asteina."""
 import requests
 
-#paikkakunta = input("Municipality name: ")
+paikkakunta = input("Municipality name: ")
+API_KEY = "" #Laita tähän oma API_KEY, koska en jaksunut tehdä git.ignore:a
 
-API_KEY =
+def get_coordinates(paikkakunta):
+    url = "http://api.openweathermap.org/geo/1.0/direct"
+    params = {
+        "q": paikkakunta,
+        "limit": 1,
+        "appid": API_KEY
+    }
+    data = requests.get(url, params=params).json()
+    return (data[0]["lat"], data[0]["lon"]) if data else (None, None)
 
-#geo = "http://api.openweathermap.org/geo/1.0/direct?q=paikkakunta,{state code},{country code}&limit={limit}&appid={}"
-#saa = "https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={}"
+lat, lon = get_coordinates(paikkakunta)
 
-joku = "https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={}"
+def get_weather(paikkakunta):
+    url = "http://api.openweathermap.org/data/2.5/weather"
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "appid": API_KEY,
+        "units": "metric"
+    }
+    data = requests.get(url, params=params).json()
+    return (data["main"]["temp"], data["weather"][0]["main"])
 
-vastaus = requests.get(joku).json()
-print(vastaus)
+print(f"Lämpötila ja säätila ovat: {get_weather(paikkakunta)}")
